df6d78c5528dc639d732d7a88e9568fced310493
Omkar Bankar <omkar080502@gmail.com>
2024-11-11 00:36:45 +0530

    Added checkout form

---
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,68 @@
+import { state } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent implements OnInit{
+
+
+  checkoutFormGroup!: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+
+  constructor(private formBuilder : FormBuilder){}
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      })
+    });
+  }
+
+  copyShippingAddressToBillingAddress(event: Event) {
+    // if(event.target)
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked){
+      this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    }
+    else{
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
+  onSubmit(){
+    console.log(this.checkoutFormGroup.value)
+  }
+
+
+}
